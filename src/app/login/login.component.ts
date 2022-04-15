66a5fece85529ebcf2386caca74f9f312e7ef301
bismisabu <bismi.bismisabuks@gmail.com>
2022-04-15 11:05:34 +0530

    two way ngmodel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -28,45 +28,70 @@ pswd=""
   ngOnInit(): void {
   }
 
-  acnoChange(event:any){
-    this.accno = event.target.value
-    console.log(event.target.value);
+  // acnoChange(event:any){
+  //   this.accno = event.target.value
+  //   console.log(event.target.value);
     
-  }
+  // }
 
-  pswdChange(event:any){
-    this.pswd = event.target.value
-    console.log(event.target.value);
+  // pswdChange(event:any){
+  //   this.pswd = event.target.value
+  //   console.log(event.target.value);
     
-  }
+  // } // event binding
 
-  login(a:any,p:any){
+  //login using event binding/ two way binding
+   login(){
+                                                                            
+         // user entered acno and pswd
+        var accno = this.accno
+        console.log(accno);
+        
+         var pass = this.pswd
+         let database = this.database
+         if(accno in database){
 
-    console.log(a.value);
+        if(pass == database[accno]["password"]){
+
+           alert("Login successfull!!! ")
+          }
+
+          else{
+             alert("invalid passsword ")
+             }
+        
+         }
+         else{
+         alert("User does not exist!!! ")
+         }
+     }
+  // login(a:any,p:any){
+
+  //   console.log(a.value);
     
  
-    // user entered acno and pswd
-    var accno = a.value
-    var pass = p.value
+  //   // user entered acno and pswd
+  //   var accno = a.value
+  //   var pass = p.value
 
-    let database = this.database
+  //   let database = this.database
 
-    if(accno in database){
+  //   if(accno in database){
 
-       if(pass == database[accno]["password"]){
+  //      if(pass == database[accno]["password"]){
 
-          alert("Login successfull!!! ")
-        }
+  //         alert("Login successfull!!! ")
+  //       }
 
-        else{
-          alert("incorrect passsword ")
-        }
+  //       else{
+  //         alert("incorrect passsword ")
+  //       }
       
-    }
-    else{
-      alert("User does not exist!!! ")   
-    }
-  }
+  //   }
+  //   else{
+  //     alert("User does not exist!!! ")   
+  //   }
+  // }
 }
 
-// 1:7:39
+// this indicate - class details point cheyyan  - call by reference
